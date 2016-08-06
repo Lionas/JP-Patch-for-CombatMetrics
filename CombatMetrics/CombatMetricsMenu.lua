a12f80881af8019cdde37aab5ce26f9588079f8a
Lionas <lionas.watanabe@gmail.com>
2016-08-06 11:20:54 +0900

    WIP:翻訳中

---
+++ CombatMetrics/CombatMetricsMenu.lua
@@ -0,0 +1,346 @@
+﻿if CMX == nil then CMX = {} end
+local CMX = CMX
+local _
+--
+-- Register with LibMenu and ESO
+
+function CMX.MakeMenu()
+    -- load the settings->addons menu library
+	local menu = LibStub("LibAddonMenu-2.0")
+	local set = CMX.set
+	local def = CMX.defaults 
+
+    -- the panel for the addons menu
+	local panel = {
+		type = "panel",
+		name = "Combat Metrics",
+		displayName = "Combat Metrics",
+		author = "Solinur",
+        version = "" .. CMX.version,
+		registerForRefresh = true,
+	}
+
+    --this adds entries in the addon menu
+	local options = {
+		{
+			type = "header",
+			name = GetString(SI_COMBAT_METRICS_MENU_PROFILES)
+		},
+		{
+			type = "checkbox",
+			name = GetString(SI_COMBAT_METRICS_MENU_AC_NAME),
+			tooltip = GetString(SI_COMBAT_METRICS_MENU_AC_TOOLTIP),
+			default = def.accountwide,
+			getFunc = function() return CombatMetrics_Save.Default[GetDisplayName()]['$AccountWide']["accountwide"] end,
+			setFunc = function(value) CombatMetrics_Save.Default[GetDisplayName()]['$AccountWide']["accountwide"] = value ReloadUI() end,
+			warning = GetString(SI_COMBAT_METRICS_MENU_UI_WARNING),
+		},		
+		{
+			type = "custom",
+		},
+		{
+			type = "header",
+			name = GetString(SI_COMBAT_METRICS_MENU_GS_NAME)
+		},
+		{
+			type = "slider",
+			name = GetString(SI_COMBAT_METRICS_MENU_CT_NAME),
+			tooltip = GetString(SI_COMBAT_METRICS_MENU_CT_TOOLTIP),
+			min = 0,
+			max = 20,
+			step = 1,
+			default = def.timeout,
+			getFunc = function() return set.timeout/1000 end,
+			setFunc = function(value) set.timeout = value*1000	end,
+		},
+		{
+			type = "slider",
+			name = GetString(SI_COMBAT_METRICS_MENU_FH_NAME),
+			tooltip = GetString(SI_COMBAT_METRICS_MENU_FH_TOOLTIP),
+			min = 1,
+			max = 25,
+			step = 1,
+			default = def.fighthistory,
+			getFunc = function() return set.fighthistory end,
+			setFunc = function(value) set.fighthistory = value end,
+		},
+		{
+			type = "checkbox",
+			name = GetString(SI_COMBAT_METRICS_MENU_UH_NAME),
+			tooltip = GetString(SI_COMBAT_METRICS_MENU_UH_TOOLTIP),
+			default = def.timeheals,
+			getFunc = function() return set.timeheals end,
+			setFunc = function(value) set.timeheals = value	end,
+		},
+		{
+			type = "checkbox",
+			name = GetString(SI_COMBAT_METRICS_MENU_MG_NAME),
+			tooltip = GetString(SI_COMBAT_METRICS_MENU_MG_TOOLTIP),
+			default = def.recordgrp,
+			getFunc = function() return set.recordgrp end,
+			setFunc = function(value) set.recordgrp = value	CMX.UI:ControlLR() end,
+		},
+		{
+			type = "checkbox",
+			name = GetString(SI_COMBAT_METRICS_MENU_AS_NAME),
+			tooltip = GetString(SI_COMBAT_METRICS_MENU_AS_TOOLTIP),
+			default = def.autoscreenshot,
+			getFunc = function() return set.autoscreenshot end,
+			setFunc = function(value) set.autoscreenshot = value end,
+		},		
+		{
+			type = "slider",
+			name = GetString(SI_COMBAT_METRICS_MENU_ML_NAME),
+			tooltip = GetString(SI_COMBAT_METRICS_MENU_ML_TOOLTIP),
+			min = 1,
+			max = 120,
+			step = 1,
+			disabled = function() return (not set.autoscreenshot) end,
+			default = def.autoscreenshotmintime,
+			getFunc = function() return set.autoscreenshotmintime end,
+			setFunc = function(value) set.autoscreenshotmintime = value end,
+		},
+		{
+			type = "slider",
+			name = "Scale of Fight Report Window",
+			tooltip = "Adjusts the size of all elements of the Fightreport Window",
+			min = 30,
+			max = (math.ceil(GuiRoot:GetHeight()/75)*10) or 200,
+			step = 10,
+			default = def.UI.ReportScale,
+			getFunc = function() return set.UI.ReportScale*100  end,
+			setFunc = function(value) 
+					set.UI.ReportScale = value/100 
+					CMX.UI:ControlRW() 	
+				end,
+		},
+		{
+			type = "custom",
+		},
+		{
+			type = "header",
+			name = "Live Report Window",
+		},
+		{
+			type = "checkbox",
+			name = "Enable",
+			tooltip = "Enable Live Report Window which shows DPS & HPS during combat",
+			default = def.EnableLiveMonitor,
+			getFunc = function() return set.EnableLiveMonitor end,
+			setFunc = function(value) CMX.UI.ToggleLR(value) set.EnableLiveMonitor = value end,
+		},
+		{
+			type = "dropdown",
+			name = "Layout",
+			tooltip = "Select the Layout of the Live Report Window",
+			default = def.UI.CMX_LiveReportSettings.layout,
+			choices = {"Compact", "Horizontal", "Vertical"},
+			getFunc = function() return set.UI.CMX_LiveReportSettings.layout end,
+			setFunc = function(value) set.UI.CMX_LiveReportSettings.layout = value CMX.UI:RefreshLRWindow() end,
+			disabled = function() return not set.EnableLiveMonitor end,
+		},
+		{
+			type = "slider",
+			name = "Scale",
+			tooltip = "Scale of the Live report window.",
+			min = 30,
+			max = 300,
+			step = 10,
+			default = def.UI.CMX_LiveReportSettings.scale,
+			getFunc = function() return set.UI.CMX_LiveReportSettings.scale*100  end,
+			setFunc = function(value) 
+					set.UI.CMX_LiveReportSettings.scale = value/100 
+					CMX.UI:ControlLR() 
+				end,
+			disabled = function() return not set.EnableLiveMonitor end,
+		},
+		{
+			type = "checkbox",
+			name = "Show Background",
+			tooltip = "Show the Background og the Live Report Window",
+			default = def.UI.CMX_LiveReportSettings.bg,
+			getFunc = function() return set.UI.CMX_LiveReportSettings.bg end,
+			setFunc = function(value) set.UI.CMX_LiveReportSettings.bg = value CMX_LiveReport_BG:SetHidden(not value) end,
+			disabled = function() return not set.EnableLiveMonitor end,
+		},
+		{
+			type = "checkbox",
+			name = "Show DPS",
+			width = "half",
+			tooltip = "Show DPS you deal in Live Report Window",
+			default = def.UI.CMX_LiveReportSettings.dps,
+			getFunc = function() return set.UI.CMX_LiveReportSettings.dps end,
+			setFunc = function(value) set.UI.CMX_LiveReportSettings.dps = value CMX.UI:RefreshLRWindow() end,
+			disabled = function() return not set.EnableLiveMonitor end,
+		},
+		{
+			type = "checkbox",
+			name = "Show HPS",
+			width = "half",
+			tooltip = "Show HPS you cast in Live Report Window",
+			default = def.UI.CMX_LiveReportSettings.hps,
+			getFunc = function() return set.UI.CMX_LiveReportSettings.hps end,
+			setFunc = function(value) set.UI.CMX_LiveReportSettings.hps = value CMX.UI:RefreshLRWindow() end,
+			disabled = function() return not set.EnableLiveMonitor end,
+		},
+		{
+			type = "checkbox",
+			name = "Show Incoming DPS",
+			width = "half",
+			tooltip = "Show DPS you receive in Live Report Window",
+			default = def.UI.CMX_LiveReportSettings.idps,
+			getFunc = function() return set.UI.CMX_LiveReportSettings.idps end,
+			setFunc = function(value) set.UI.CMX_LiveReportSettings.idps = value CMX.UI:RefreshLRWindow() end,
+			disabled = function() return not set.EnableLiveMonitor end,
+		},
+		{
+			type = "checkbox",
+			name = "Show Incoming HPS",
+			width = "half",
+			tooltip = "Show HPS you receive in Live Report Window",
+			default = def.UI.CMX_LiveReportSettings.ihps,
+			getFunc = function() return set.UI.CMX_LiveReportSettings.ihps end,
+			setFunc = function(value) set.UI.CMX_LiveReportSettings.ihps = value CMX.UI:RefreshLRWindow() end,
+			disabled = function() return not set.EnableLiveMonitor end,
+		},
+		{
+			type = "checkbox",
+			name = "Show Time",
+			width = "half",
+			tooltip = "Show Time you have been dealing damage in Live Report Window",
+			default = def.UI.CMX_LiveReportSettings.time,
+			getFunc = function() return set.UI.CMX_LiveReportSettings.time end,
+			setFunc = function(value) set.UI.CMX_LiveReportSettings.time = value CMX.UI:RefreshLRWindow() end,
+			disabled = function() return not set.EnableLiveMonitor end,
+		},
+		{
+			type = "custom",
+			width = "half",
+			
+		},
+		{
+			type = "custom",
+		},
+		{
+			type = "header",
+			name = "Stream Combat Log to chat",
+		},
+		{
+			type = "checkbox",
+			name = "Enable",
+			tooltip = "Streams Damage and Heal Events to chat window",
+			default = def.EnableChatLog,
+			getFunc = function() return set.EnableChatLog end,
+			setFunc = function(value) if value then CMX.getCombatLog() end set.EnableChatLog = value end,
+		},
+		{
+			type = "checkbox",
+			name = "Show damage",
+			tooltip = "Show damage you deal in chat stream",
+			default = def.EnableChatLogDmgOut,
+			getFunc = function() return set.EnableChatLogDmgOut end,
+			setFunc = function(value) set.EnableChatLogDmgOut = value end,
+			disabled = function() return not set.EnableChatLog end,
+		},
+		{
+			type = "checkbox",
+			name = "Show heals",
+			tooltip = "Show heals you cast in chat stream",
+			default = def.EnableChatLogHealOut,
+			getFunc = function() return set.EnableChatLogHealOut end,
+			setFunc = function(value) set.EnableChatLogHealOut = value end,
+			disabled = function() return not set.EnableChatLog end,
+		},
+		{
+			type = "checkbox",
+			name = "Show Incoming damage",
+			tooltip = "Show damage you receive in chat stream",
+			default = def.EnableChatLogDmgIn,
+			getFunc = function() return set.EnableChatLogDmgIn end,
+			setFunc = function(value) set.EnableChatLogDmgIn = value end,
+			disabled = function() return not set.EnableChatLog end,
+		},
+		{
+			type = "checkbox",
+			name = "Show Incoming heal",
+			tooltip = "Show heals you receive in chat stream",
+			default = def.EnableChatLogHealIn,
+			getFunc = function() return set.EnableChatLogHealIn end,
+			setFunc = function(value) set.EnableChatLogHealIn = value end,
+			disabled = function() return not set.EnableChatLog end,
+		},
+		{
+			type = "custom",
+		},
+		{
+			type = "header",
+			name = "Debug options",
+		},
+		{
+			type = "checkbox",
+			name = "Show Fight Recap",
+			tooltip = "Print Combat Results to the system chat window",
+			default = def.debuginfo.fightsummary,
+			getFunc = function() return set.debuginfo.fightsummary end,
+			setFunc = function(value) set.debuginfo.fightsummary = value end,
+			width = "half",
+		},
+		{
+			type = "checkbox",
+			name = "Show ability IDs",
+			tooltip = "Show ability ids in the fight report window",
+			default = def.debuginfo.ids,
+			getFunc = function() return set.debuginfo.ids end,
+			setFunc = function(value) set.debuginfo.ids = value	end,
+			width = "half",
+		},
+		{
+			type = "checkbox",
+			name = "Show Fight Calculation Info",
+			tooltip = "Print Info about the calculation timings to the system chat window",
+			default = def.debuginfo.calculationtime,
+			getFunc = function() return set.debuginfo.calculationtime end,
+			setFunc = function(value) set.debuginfo.calculationtime = value	end,
+			width = "half",
+		},
+		{
+			type = "checkbox",
+			name = "Show Buff Info",
+			tooltip = "Print Buff events to the system chat window (Spammy)",
+			default = def.debuginfo.buffs,
+			getFunc = function() return set.debuginfo.buffs end,
+			setFunc = function(value) set.debuginfo.buffs = value end,
+			width = "half",
+		},
+		{
+			type = "checkbox",
+			name = "Show used Skill Info",
+			tooltip = "Print used skill events to the system chat window",
+			default = def.debuginfo.skills,
+			getFunc = function() return set.debuginfo.skills end,
+			setFunc = function(value) set.debuginfo.skills = value end,
+			width = "half",
+		},
+		{
+			type = "checkbox",
+			name = "Show group Info",
+			tooltip = "Print group joining and leave events to the system chat window",
+			default = def.debuginfo.group,
+			getFunc = function() return set.debuginfo.group end,
+			setFunc = function(value) set.debuginfo.group = value end,
+			width = "half",
+		},
+		{
+			type = "checkbox",
+			name = "Show miscellaneous debug Info",
+			tooltip = "Print some other events to the system chat window",
+			default = def.debuginfo.misc,
+			getFunc = function() return set.debuginfo.misc end,
+			setFunc = function(value) set.debuginfo.misc = value end,
+			width = "half",
+		},
+	}
+
+	menu:RegisterAddonPanel("CMX_Options", panel)
+	menu:RegisterOptionControls("CMX_Options", options)
+end
---
+++ CombatMetrics/lang/jp.lua
@@ -0,0 +1,180 @@
+﻿ZO_CreateStringId("SI_COMBAT_METRICS_LANG", "jp")
+
+-- UI&Control
+ZO_CreateStringId("SI_COMBAT_METRICS_FONT_SIZE", "11") -- 14
+
+ZO_CreateStringId("SI_COMBAT_METRICS_SHOW_XPS", "<<1>> / <<2>> (<<3>>%)")
+ZO_CreateStringId("SI_COMBAT_METRICS_CALC", "計算中...")
+ZO_CreateStringId("SI_COMBAT_METRICS_GROUP", "グループ")
+ZO_CreateStringId("SI_COMBAT_METRICS_SELECTION", "選択")
+
+ZO_CreateStringId("SI_COMBAT_METRICS_BASE_REG", "基本再生")
+ZO_CreateStringId("SI_COMBAT_METRICS_UNKNOWN", "不明")
+
+ZO_CreateStringId("SI_COMBAT_METRICS_BLOCKS", "Blocks")
+ZO_CreateStringId("SI_COMBAT_METRICS_CRITS", "クリ")
+
+ZO_CreateStringId("SI_COMBAT_METRICS_DAMAGE", "ダメージ") -- Damage
+ZO_CreateStringId("SI_COMBAT_METRICS_HIT", "ヒット")
+ZO_CreateStringId("SI_COMBAT_METRICS_DPS", "DPS")
+
+ZO_CreateStringId("SI_COMBAT_METRICS_HEALING", "回復")
+ZO_CreateStringId("SI_COMBAT_METRICS_HEALS", "回復数")
+ZO_CreateStringId("SI_COMBAT_METRICS_HPS", "HPS")
+
+ZO_CreateStringId("SI_COMBAT_METRICS_INC_DAMAGE", "被ダメージ")
+ZO_CreateStringId("SI_COMBAT_METRICS_INC_HITS", "被ヒット")
+ZO_CreateStringId("SI_COMBAT_METRICS_INC_DPS", "被DPS") -- Inc DPS
+
+ZO_CreateStringId("SI_COMBAT_METRICS_INC_HEALING", "被回復")
+ZO_CreateStringId("SI_COMBAT_METRICS_INC_HEALS", "被回復数")
+ZO_CreateStringId("SI_COMBAT_METRICS_INC_HPS", "被HPS") -- Inc HPS
+
+ZO_CreateStringId("SI_COMBAT_METRICS_DAMAGE_CAUSED", "与えたダメージ") -- Damage Caused
+ZO_CreateStringId("SI_COMBAT_METRICS_DAMAGE_RECIEVED", "受けたダメージ") -- Damage Received
+ZO_CreateStringId("SI_COMBAT_METRICS_HEALING_DONE", "行った回復") -- Healing Done
+ZO_CreateStringId("SI_COMBAT_METRICS_HEALING_RECIVED", "被回復") -- Healing Done
+ZO_CreateStringId("SI_COMBAT_METRICS_FIGHT_REPORT", "戦闘結果") -- Fight Report
+ZO_CreateStringId("SI_COMBAT_METRICS_TOGGLE_COMBAT_LOG", "戦闘ログの切り替え") -- Toggle Combat Log
+
+ZO_CreateStringId("SI_COMBAT_METRICS_DELETE_COMBAT_LOG", "戦闘ログの削除") -- Delete Combat Log
+ZO_CreateStringId("SI_COMBAT_METRICS_DELETE_FIGHT", "戦闘の削除") -- Delete Combat Log
+
+ZO_CreateStringId("SI_COMBAT_METRICS_STORAGE_FULL", "ストレージファイルが一杯です。容量確保のため戦闘を削除してください！") -- The storage file is full. Delete a fight to free some space!
+
+ZO_CreateStringId("SI_COMBAT_METRICS_PREVIOUS_FIGHT", "前回の戦闘") -- Previuos Fight
+ZO_CreateStringId("SI_COMBAT_METRICS_NEXT_FIGHT", "次の戦闘") -- Next Fight
+ZO_CreateStringId("SI_COMBAT_METRICS_MOST_RECENT_FIGHT", "最近の戦闘") -- Most Recent Fight
+ZO_CreateStringId("SI_COMBAT_METRICS_LOAD_FIGHT", "戦闘のロード") -- Load Fight
+ZO_CreateStringId("SI_COMBAT_METRICS_SAVE_FIGHT", "戦闘の保存") -- Save Fight
+ZO_CreateStringId("SI_COMBAT_METRICS_DELETE_FIGHT", "戦闘の削除") -- Delete Fight
+
+ZO_CreateStringId("SI_COMBAT_METRICS_RECENT_FIGHT", "最近の戦闘") -- Recent Fight
+ZO_CreateStringId("SI_COMBAT_METRICS_DURATION", "期間") -- Duration
+ZO_CreateStringId("SI_COMBAT_METRICS_SHOW", "表示") -- Show
+ZO_CreateStringId("SI_COMBAT_METRICS_DELETE", "削除") -- Delete
+
+ZO_CreateStringId("SI_COMBAT_METRICS_SAVED_FIGHTS", "保存された戦闘") -- Saved Fights
+
+ZO_CreateStringId("SI_COMBAT_METRICS_ACTIVE_TIME", "有効時間: ") -- Active Time: 
+ZO_CreateStringId("SI_COMBAT_METRICS_ZERO_SEC", "0 秒") -- 0 s
+ZO_CreateStringId("SI_COMBAT_METRICS_IN_COMBAT", "戦闘中: ") -- In Combat: 
+
+ZO_CreateStringId("SI_COMBAT_METRICS_PLAYER", "プレイヤー") -- Player
+
+ZO_CreateStringId("SI_COMBAT_METRICS_DPS_C", "DPS: ") -- DPS: 
+
+ZO_CreateStringId("SI_COMBAT_METRICS_TOTAL", "合計: ") -- Total: 
+ZO_CreateStringId("SI_COMBAT_METRICS_NORMAL", "通常: ") -- Normal: 
+ZO_CreateStringId("SI_COMBAT_METRICS_CRITICAL", "クリティカル: ") -- Critical: 
+ZO_CreateStringId("SI_COMBAT_METRICS_BLOCKED", "ブロック: ") -- Blocked: 
+ZO_CreateStringId("SI_COMBAT_METRICS_SHIELDED", "シールド: ") -- Shielded: 
+
+ZO_CreateStringId("SI_COMBAT_METRICS_HITS", "ヒット") -- Hits
+
+ZO_CreateStringId("SI_COMBAT_METRICS_RESOURCES", "リソース")  -- Resources
+ZO_CreateStringId("SI_COMBAT_METRICS_REGS", "再生/秒")  -- Regs/s
+ZO_CreateStringId("SI_COMBAT_METRICS_DRAINS", "吸収/秒")  -- Drain/s
+
+ZO_CreateStringId("SI_COMBAT_METRICS_MAGICKA_C", "マジカ:")  -- Magicka:
+ZO_CreateStringId("SI_COMBAT_METRICS_STAMINA_C", "スタミナ:")  -- Stamina:
+ZO_CreateStringId("SI_COMBAT_METRICS_ULTIMATE_C", "アルティメット:")  -- Ultimate:
+
+ZO_CreateStringId("SI_COMBAT_METRICS_STATS", "統計")  -- Stats
+ZO_CreateStringId("SI_COMBAT_METRICS_AVE", "平均")  -- Average
+ZO_CreateStringId("SI_COMBAT_METRICS_MAX", "最大")  -- Maximum
+ZO_CreateStringId("SI_COMBAT_METRICS_MAX_MAGICKA_C", "最大マジカ:")  -- MaxMagicka:
+ZO_CreateStringId("SI_COMBAT_METRICS_SPELL_DMG_C", "呪文ダメージ:")  -- Spell Dmg:
+ZO_CreateStringId("SI_COMBAT_METRICS_SPELL_CRIT_C", "呪文クリティ:")  -- Spell Crit:
+ZO_CreateStringId("SI_COMBAT_METRICS_MAX_STAMINA_C", "最大スタミナ:")  -- MaxStamina:
+ZO_CreateStringId("SI_COMBAT_METRICS_WEAPON_DMC_C", "武器ダメージ:")  -- Weapon Dmg:
+ZO_CreateStringId("SI_COMBAT_METRICS_WEAPON_CRIT_C", "武器クリティ:")  -- Weaponcrit:
+ZO_CreateStringId("SI_COMBAT_METRICS_PHYSICAL_RES_C", "物理レジスト:")  -- Physical Res:
+ZO_CreateStringId("SI_COMBAT_METRICS_SPELL_RES_C", "呪文レジスト:")  -- Spell Res:
+ZO_CreateStringId("SI_COMBAT_METRICS_CRITICAL_RES_C", "クリレジスト:")  -- Critical Res:
+
+ZO_CreateStringId("SI_COMBAT_METRICS_COMBAT_LOG", "戦闘ログ")  -- Combat Log
+
+ZO_CreateStringId("SI_COMBAT_METRICS_GOTO_PREVIOUS", "前のページへ")  -- Go to previous pages
+ZO_CreateStringId("SI_COMBAT_METRICS_PAGE", "ページ <<1>>")  -- Page 
+ZO_CreateStringId("SI_COMBAT_METRICS_GOTO_NEXT", "次のページへ")  -- Go to next pages
+
+ZO_CreateStringId("SI_COMBAT_METRICS_TOGGLE_HEAL", "受けた回復のイベントを切り替える")  -- Toggle received heal events
+ZO_CreateStringId("SI_COMBAT_METRICS_TOGGLE_DAMAGE", "受けたダメージのイベントを切り替える")  -- Toggle received damage events
+
+ZO_CreateStringId("SI_COMBAT_METRICS_TOGGLE_YOUR_HEAL", "あなたが回復したイベントを切り替える")  -- Toggle your healing events
+ZO_CreateStringId("SI_COMBAT_METRICS_TOGGLE_YOUR_DAMAGE", "あなたのダメージイベントを切り替える")  -- Toggle your damage events
+ZO_CreateStringId("SI_COMBAT_METRICS_TOGGLE_BUFF_EVENTS", "効果のイベントを切り替える")  -- Toggle buff events
+ZO_CreateStringId("SI_COMBAT_METRICS_TOGGLE_RESOURCE_EVENTS", "リソースのイベントを切り替える")  -- Toggle resource events
+ZO_CreateStringId("SI_COMBAT_METRICS_TOGGLE_STATS_CHANGE_EVENTS", "統計の変化イベントを切り替える")  -- Toggle stats change events
+
+ZO_CreateStringId("SI_COMBAT_METRICS_DEBUFF", "（デ）バフ")  -- (De-)Buffs
+ZO_CreateStringId("SI_COMBAT_METRICS_DEBUFF_OUT", "（デ）バフ\nアウト")  -- (De-)Buffs\nOut
+ZO_CreateStringId("SI_COMBAT_METRICS_MAGICKA_PM", "マジカ\n +/-")  -- Magicka\n +/-
+ZO_CreateStringId("SI_COMBAT_METRICS_STAMINA_PM", "スナミナ\n +/-")  -- Stamina\n +/-
+
+ZO_CreateStringId("SI_COMBAT_METRICS_BUFF", "バフ")  -- Buff
+ZO_CreateStringId("SI_COMBAT_METRICS_SHARP", "#")  -- #
+ZO_CreateStringId("SI_COMBAT_METRICS_UPTIME", "稼働時間")  -- Uptime
+
+ZO_CreateStringId("SI_COMBAT_METRICS_SOURCE", " ソース")  --  Source
+ZO_CreateStringId("SI_COMBAT_METRICS_PM_SEC", "±/秒")  -- ±/s
+ZO_CreateStringId("SI_COMBAT_METRICS_TARGET", "ターゲット")  -- Target
+ZO_CreateStringId("SI_COMBAT_METRICS_PERCENT", "%")  -- %
+
+ZO_CreateStringId("SI_COMBAT_METRICS_AVILITY", "アビリティ")  -- Ability
+ZO_CreateStringId("SI_COMBAT_METRICS_PER_HITS", "/ヒット")  -- Crits/Hits
+ZO_CreateStringId("SI_COMBAT_METRICS_CRITS_PER", "クリ %")  -- Crit %
+
+
+-- Menus
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_PROFILES", "プロファイル")  -- Profiles
+
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_AC_NAME", "アカウント全体設定を使用")  -- Use accountwide settings
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_AC_TOOLTIP", "有効な場合アカウントの全てのキャラクターで設定を共有する")  -- If enabled all chars of an account will share their settings
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_UI_WARNING", "このオプションを変更するとUIの再読み込みが行われます")  -- Changing this option will trigger a reload of the user interface
+
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_GS_NAME", "一般設定")  -- General Settings
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_CT_NAME", "戦闘タイムアウト")  -- Combat Timeout
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_CT_TOOLTIP", "戦闘終了時にDPSをリセットする攻撃の間の秒数")  -- Time in s between your attacks to reset the DPS (when out of combat)
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_FH_NAME", "戦闘履歴")  -- Fight History
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_FH_TOOLTIP", "保存する最近の戦闘の数")  -- Number of recent fights to save
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_UH_NAME", "タイマーのヒールを使用する")  -- Use heals for timers?
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_UH_TOOLTIP", "dps/hps計算のためにトリガ時間のヒールを使用する")  -- Use heals to trigger time (for calculating dps/hps)
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_MG_NAME", "グループダメージを監視")  -- Monitor Group Damage
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_MG_TOOLTIP", "グループ全体のイベントを監視する")  -- Monitor the events of the whole group
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_AS_NAME", "自動スクリーンショット")  -- Auto Screenshot
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_AS_TOOLTIP", "レポート画面を開いた時に自動的にスクリーンショットを撮る")  -- Automatically take a Screenshot when opening the Report window
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_ML_NAME", "スクリーンショットの最小戦闘期間")  -- Minimum fight length for screenshot
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_ML_TOOLTIP", "自動スクリーンショットを撮る最小の戦闘の期間（秒）")  -- "Minimum fight length in s for auto screenshot
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
+ZO_CreateStringId("SI_COMBAT_METRICS_MENU_", "")  -- 
